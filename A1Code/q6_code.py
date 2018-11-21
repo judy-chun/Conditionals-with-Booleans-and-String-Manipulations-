39d0fc912dfcb80f02df74d75a7abc378fe1b315
judy-chun <35321574+judy-chun@users.noreply.github.com>
2018-11-21 15:48:19 -0800

    Add files via upload

---
+++ A1Code/q6_code.py
@@ -0,0 +1,9 @@
+# ^ You can ignore this line - it is used to help check your code
+
+if status == True:
+    output = "GOOD"
+else: 
+    output = "BAD"
+
+# YOUR CODE HERE
+# raise NotImplementedError()
---
+++ A1Code/q7_code.py
@@ -0,0 +1,9 @@
+# ^ You can ignore this line - it is used to help check your code
+
+if subj_age == "old":
+    change_analysis = True
+else: 
+    change_analysis = False
+    
+# YOUR CODE HERE
+# raise NotImplementedError()
